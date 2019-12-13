004314e70fa9fd877432e8fbf60247af45d89b07
Liliya Gavrishchishena <Liliya.Gavrishchishena@gmail.com>
2019-12-13 16:46:38 +0200

    menu

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,4 @@
+// active link
 const form = document.getElementById('nav__list');
 let btns = form.getElementsByClassName('nav__link');
 
@@ -15,3 +16,18 @@ for (let i = 0; i < btns.length; i += 1) {
     this.className += ' nav__link--active';
   });
 }
+
+// responsive menu
+
+const burger = document.querySelector('.burger i');
+const nav = document.querySelector('.nav__list');
+
+function toggleNav() {
+  burger.classList.toggle('fa-bars');
+  burger.classList.toggle('fa-times');
+  nav.classList.toggle('nav__list--active');
+}
+
+burger.addEventListener('click', function() {
+  toggleNav();
+});
