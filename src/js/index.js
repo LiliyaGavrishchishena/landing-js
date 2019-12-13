fde5eb7c668723ecc74f7bb6ddb36369d822583a
Liliya Gavrishchishena <Liliya.Gavrishchishena@gmail.com>
2019-12-13 12:12:42 +0200

    active link added

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,17 @@
 const form = document.getElementById('nav__list');
 let btns = form.getElementsByClassName('nav__link');
-for (let i = 0; i < btns.length; i++) {
+
+for (let i = 0; i < btns.length; i += 1) {
   btns[i].addEventListener('click', function() {
     let current = document.getElementsByClassName('nav__link--active');
-    current[0].className = current[0].className.replace(
-      ' nav__link--active',
-      '',
-    );
+
+    if (current.length > 0) {
+      current[0].className = current[0].className.replace(
+        ' nav__link--active',
+        '',
+      );
+    }
+
     this.className += ' nav__link--active';
   });
 }
