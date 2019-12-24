121f8d624bcad5ea45e411ffc2781f6585654342
Liliya Gavrishchishena <Liliya.Gavrishchishena@gmail.com>
2019-12-24 10:41:05 +0200

    Drag & Drop

--- src/js/index.js
+++ src/js/index.js
@@ -31,3 +31,62 @@ function toggleNav() {
 burger.addEventListener('click', function() {
   toggleNav();
 });
+
+//Drag & Drop List Items
+let dragSrcEl = null;
+
+function handleDragStart(e) {
+  this.style.opacity = '0.4';
+  dragSrcEl = this;
+
+  e.dataTransfer.effectAllowed = 'move';
+  e.dataTransfer.setData('text/html', this.innerHTML);
+}
+
+function handleDragOver(e) {
+  if (e.preventDefault) {
+    e.preventDefault();
+  }
+
+  e.dataTransfer.dropEffect = 'move';
+
+  return false;
+}
+
+function handleDragEnter(e) {
+  this.classList.add('over');
+}
+
+function handleDragLeave(e) {
+  this.classList.remove('over');
+}
+
+function handleDrop(e) {
+  if (e.stopPropagation) {
+    e.stopPropagation();
+  }
+
+  if (dragSrcEl != this) {
+    dragSrcEl.innerHTML = this.innerHTML;
+    this.innerHTML = e.dataTransfer.getData('text/html');
+  }
+
+  return false;
+}
+
+function handleDragEnd(e) {
+  this.style.opacity = '1';
+  [].forEach.call(cols, function(col) {
+    col.classList.remove('over');
+  });
+}
+
+let cols = document.querySelectorAll('#service__list .service__list--item');
+[].forEach.call(cols, function(col) {
+  col.addEventListener('dragstart', handleDragStart, false);
+  col.addEventListener('dragenter', handleDragEnter, false);
+  col.addEventListener('dragover', handleDragOver, false);
+  col.addEventListener('dragleave', handleDragLeave, false);
+  col.addEventListener('drop', handleDrop, false);
+  col.addEventListener('dragend', handleDragEnd, false);
+});
